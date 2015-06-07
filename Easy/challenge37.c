2ec31fca356d5765c494121ae9fea64291d44d79
Josh <joshvocal@gmail.com>
2015-06-07 07:48:00 -0700

    added challenge37

---
+++ Easy/challenge37.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define TRUE   1
+#define FALSE  0 
+
+int main()
+{
+  unsigned long wordcount = 0, linecount = 0, charcount = 0;
+  int c, flag;
+
+  flag = FALSE;
+  linecount = wordcount = charcount = 0;
+  while ((c = getchar()) != EOF)
+  {
+    ++charcount;
+    if (c == '\n')
+    {
+      ++linecount;
+    }
+      
+    if (c == ' ' || c == '\t' || c == '\n' || c == '\0' || (isalpha(c) == 0 && c != '\''))
+    {
+      flag = FALSE;
+    }
+    else if (flag == FALSE)
+    {
+      flag = TRUE;
+      ++wordcount;
+    }
+  }
+  printf( "%lu %lu %lu\n", charcount, wordcount, linecount );
+  return 0;
+}
